d702bd347cfb006c679f78ea4c94d0c1ac662376
PankajC-Tft <pankajc@applause.stream>
2022-09-18 20:04:49 +0530

    first commit

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'poet snow spare call need audit know stuff swing layer tuna pitch',
+  'https://rinkeby.infura.io/v3/3208005a0dbc47e398ee20685d653e76'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
